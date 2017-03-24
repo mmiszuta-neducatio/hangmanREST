766e3f037f4c1a59d9fa242f077bd4151f3a95c8
mmiszuta-neducatio <mmiszuta@n-educatio.pl>
2017-03-24 14:08:54 +0100

    fixed bugs in hangman, added server functionalities and configured it with redis @mzielinski-neducatio @mmiszuta-neducatio

--- hangman.js
+++ hangman.js
@@ -1,20 +1,34 @@
 'use strict';
 
 let Hangman = function Hangman(word, attemptedLetters, guessedLetters, attempts) {
-  this.word = word.toUpperCase();
+  word = word.toUpperCase();
+  word.split('');
+  this.word = word;
   this.attemptedLetters = attemptedLetters || [];
-  this.guessedLetters = guessedLetters || [];
+  this.guessedLetters = guessedLetters || new Array(word.length);
+  if (word.indexOf('-') !== -1) {
+    this.guessedLetters[word.indexOf('-')] = '-';
+  }
   this.attempts = attempts || 0;
 };
 
 Hangman.prototype = {
   maxAttempts: 6,
 
+  isWordGuessed: function(guessed, word) {
+    for(let i = guessed.length; i--;) {
+      if(guessed[i] !== word[i]){
+        return false;
+      }
+    }
+    return true;
+  },
+
   isGameOver: function() {
     if (this.attempts >= this.maxAttempts) {
       return { 'isGameOver': true, 'gameOverType': 'loss' };
     }
-    if (this.guessedLetters.join('') === this.word) {
+    if (this.isWordGuessed(this.guessedLetters, this.word)) {
       return { 'isGameOver': true, 'gameOverType': 'win' };
     }
     return { 'isGameOver': false };
@@ -24,7 +38,7 @@ Hangman.prototype = {
     let isValid = false;
     letter = letter.toUpperCase();
 
-    if (!this.attemptedLetters.includes(letter)) {
+    if (this.attemptedLetters.indexOf(letter) === -1) {
       this.attemptedLetters.push(letter);
 
       for(let i = 0; i < this.word.length; i++) {
@@ -33,12 +47,15 @@ Hangman.prototype = {
           this.guessedLetters[i] = letter;
         }
       }
-      this.attempts = this.attempts + 1;
+      if (!isValid) {
+        this.attempts = this.attempts + 1;
+      }
     }
     let response = this.isGameOver();
     response.isValid = isValid;
     response.guessedLetters = this.guessedLetters;
     response.attemptedLetters = this.attemptedLetters;
+    response.remainingAttempts = this.maxAttempts - this.attempts;
     return response;
   }
 };
---
+++ server.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const express     = require('express');
+const app         = express();
+const session     = require('express-session');
+const RedisStore  = require('connect-redis')(session);
+const Hangman     = require('./hangman');
+const redis       = require('redis');
+const animals     = require('animals');
+
+const port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.listen(port, function () {
+  console.log('listening on port: ', port);
+});
+
+let client = redis.createClient('6379', 'redis');
+client.on('error', function(err){
+  console.error('error' + err);
+});
+
+
+app.use(session({
+  store: new RedisStore({
+    client: client
+  }),
+  secret: 'I have no memory of this place',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+let activeGame;
+let gameStats = {
+   'won': 0,
+  'lost': 0
+};
+
+client.exists('gameStats', function(err, reply){
+  if (err) console.error(err);
+  if (reply === 1){
+    client.hget('gameStats', 'won', function(err, reply){
+      if (err) console.error(err);
+      gameStats.won = reply;
+    });
+  } else {
+    client.hmset('gameStats', {
+      'won': 0,
+      'lost': 0
+    });
+  }
+});
+
+
+app.get('/game', function(req, res){
+  if(req.session && req.session.gameOverType){
+    loadGame(req.session.game);
+    res.send({
+      'guessedLetters': activeGame.guessedLetters,
+      'attemptedLetters': activeGame.attemptedLetters,
+      'remainingAttempts': activeGame.maxAttempts - activeGame.attempts
+    });
+  } else {
+    newGame(req.session, function(guessedLetters){
+      res.send({
+        'guessedLetters': guessedLetters,
+        'gameStats': gameStats
+      });
+    });
+  }
+});
+
+app.get('/letter/:letter', function(req, res){
+  if(req.session.game){
+    loadGame(req.session.game);
+    let json = activeGame.validateLetter(req.params.letter);
+    req.session.game = activeGame;
+    req.session.save();
+
+    if(json.isGameOver){
+      req.session.game = null;
+      req.session.save();
+      if(json.gameOverType === 'win'){
+        gameStats.won++;
+        client.hincrby('gameStats', 'won', 1);
+      } else {
+        gameStats.lost++;
+        client.hincrby('gameStats', 'lost', 1);
+      }
+    }
+    json.gameStats = gameStats;
+    res.send(json);
+  } else {
+    res.status(412).end();
+  }
+});
+
+
+function newGame(session, callback){
+  let body = animals();
+  body = body.replace(/(\r\n|\n|\r)/gm, '');
+  let game = new Hangman(body);
+  session.game = game;
+  session.save();
+  callback(session.game.guessedLetters);
+}
+
+function loadGame(gameSession){
+  activeGame = new Hangman(gameSession.word, gameSession.attemptedLetters, gameSession.guessedLetters, gameSession.attempts);
+};
